79ead6fe2b938d3e1ed0b2943db0875e3b39871b
Javier Heisecke <javierheiseckee@gmail.com>
2020-03-06 00:22:47 -0300

    Agrega dashboard y tabla con proyectos

---
+++ src/components/vue-table.vue
@@ -0,0 +1,31 @@
+<template>
+<v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="itemsPerPage"
+    class="elevation-1">
+
+    <template v-slot:item.estate="{ item }">
+      <v-chip :color="getColor(item.estate)" dark>{{ item.estate }}</v-chip>
+    </template>
+</v-data-table>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+      headers: [],
+      itemsPerPage: String,
+      items: [],
+
+    },
+    methods: {
+      getColor (estate) {
+        if (estate == "ANÁLISIS") return 'red'
+        else if (estate == "EN CURSO") return 'blue'
+        else return 'green'
+      },
+    },
+  }
+</script>
--- src/views/TraceabilityMenu.vue
+++ src/views/TraceabilityMenu.vue
@@ -7,20 +7,28 @@
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
+            <v-icon>mdi-settings</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>Modulo de Gestion de Configuración</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
+            <v-icon>mdi-account-multiple</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title>Módulo de Administración</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-source-merge</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Módulo de Desarrollo</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>        
       </v-list>
     </v-navigation-drawer>
 
@@ -30,68 +38,86 @@
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Project Traceability</v-toolbar-title>
+      <template>
+        <v-spacer />   
+          <v-btn color="primary"><v-icon>mdi-logout</v-icon>SALIR</v-btn>
+      </template>
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
+      <vueTable :headers="headers" :items="desserts" :itemsPerPage="itemsPerPage"></vueTable>
     </v-content>
     <v-footer
       color="indigo"
       app
     >
-      <span class="white--text">&copy; 2019</span>
+      <span class="white--text">&copy; Facultad Politecnica - LCIK - 2020</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+  import vueTable from '@/components/vue-table.vue';
   export default {
+    components: {
+        vueTable
+    },
     props: {
       source: String,
     },
     data: () => ({
       drawer: null,
+      itemsPerPage: 5,
+      headers: [
+          {
+            text: 'Código',
+            align: 'start',
+            sortable: false,
+            value: 'projectCode',
+          },
+          { text: 'Proyecto', value: 'projectName' },
+          { text: 'Estado', value: 'estate' },
+          { text: 'Acciones', value: 'actions' },
+      ],
+      desserts: [
+        {
+          projectCode: '1',
+          projectName: 'Aplicacion iOS para BNF',
+          estate: 'EN CURSO',
+          actions: 'XXX',
+        },
+        {
+          projectCode: '2',
+          projectName: 'Herramienta de Evaluacion de PYMES',
+          estate: 'EN CURSO',
+          actions: 'XXX',
+        },
+        {
+          projectCode: '3',
+          projectName: 'Pago de servicios automático',
+          estate: 'EN PRODUCCIÓN',
+          actions: 'XXX',
+        },
+        {
+          projectCode: '4',
+          projectName: 'Herramienta CRM 2.0',
+          estate: 'EN PRODUCCIÓN',
+          actions: 'XXX',
+        },
+        {
+          projectCode: '5',
+          projectName: 'Encuestas para clientes de la web',
+          estate: 'ANÁLISIS',
+          actions: 'XXX',
+        },
+        {
+          projectCode: '6',
+          projectName: 'Prestamos desde ATM',
+          estate: 'EN PRODUCCIÓN',
+          actions: 'XXX',
+        },        
+      ],
     }),
   }
 </script>
