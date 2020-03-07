7b5eb25c4620f4947907b5c6a8ecf21a7b20640c
Javier Heisecke <javierheiseckee@gmail.com>
2020-03-06 21:04:40 -0300

    Agrega botones al dashboard por proyecto

--- src/components/vue-table.vue
+++ src/components/vue-table.vue
@@ -8,6 +8,21 @@
     <template v-slot:item.estate="{ item }">
       <v-chip :color="getColor(item.estate)" dark>{{ item.estate }}</v-chip>
     </template>
+    <template v-slot:item.actions="{ item }">
+          <tr>
+            <td>
+                <v-btn class="mx-1" fab dark small color="blue" @click="viewProject(item.projectCode)">
+                    <v-icon dark>mdi-eye</v-icon>
+                </v-btn>                 
+                <v-btn class="mx-1" fab dark small color="blue" @click="editProject(item.projectCode)">
+                    <v-icon dark>mdi-lead-pencil</v-icon>
+                </v-btn>                  
+                <v-btn class="mx-1" fab dark small color="blue" @click="seeProjectUsers(item.projectCode)">
+                    <v-icon dark>mdi-account-edit</v-icon>
+                </v-btn>                           
+            </td>
+          </tr>
+    </template>
 </v-data-table>
 </template>
 
@@ -18,13 +33,21 @@
       headers: [],
       itemsPerPage: String,
       items: [],
-
     },
     methods: {
       getColor (estate) {
         if (estate == "ANÁLISIS") return 'red'
         else if (estate == "EN CURSO") return 'blue'
         else return 'green'
+      },
+      editProject (codigo) {
+        alert(`estas editando el proyecto n°${codigo}`)
+      },
+      seeProjectUsers (codigo) {
+        alert(`estas viendo los usuarios del proyecto n°${codigo}`)
+      },
+      viewProject (codigo) {
+        alert(`estas viendo el proyecto n°${codigo}`)
       },
     },
   }
