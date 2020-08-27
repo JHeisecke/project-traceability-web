5c2749942e4b703d5cb104ba61a93e9fceab700f
Javier Heisecke <javierheiseckee@gmail.com>
2020-08-26 22:27:36 -0400

    Crea pantalla incial de Permisos

---
+++ src/components/administration/vue-table-permiso.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <loadingDialog :loadingMessage="loadingMessage" :loadingDialogShow="loadingDialogShow"></loadingDialog>
+    <v-data-table
+      :headers="headers"
+      :items="listaroles"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>PERMISOS</v-toolbar-title>
+          <!-- Barra Vertical -->
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </template>
+    </v-data-table>  
+  </div>
+
+</template>
+
+<script>
+//const axios = require('axios');
+import loadingDialog from '@/components/loading-dialog.vue';
+
+  export default {
+    data: () => ({
+      loadingDialogShow : false,
+      loadingMessage  : "",
+      editMode: true
+    }),
+    components : {
+      loadingDialog
+    },
+  }
+</script>
+    
